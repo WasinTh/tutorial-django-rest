f32ec404294ed1988e25c592433100a591b22b75
Terry <wasin.th@gmail.com>
2021-04-04 15:31:07 +0700

    Update current-balance API to JSON

--- account/views.py
+++ account/views.py
@@ -1,13 +1,14 @@
+import json
 from django.http import HttpResponse
 from account.models import Transaction
 
 
 def current_balance_view(request):
-    balance = 0
+    data = {'balance': 0}
     for transaction in Transaction.objects.all():
         if transaction.type == Transaction.Type.INCOME:
-            balance += transaction.amount
+            data['balance'] += transaction.amount
         else:
-            balance -= transaction.amount
+            data['balance'] -= transaction.amount
 
-    return HttpResponse(f"{balance} บาท")
+    return HttpResponse(json.dumps(data))
